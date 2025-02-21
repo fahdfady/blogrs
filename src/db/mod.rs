b0523db6120ada56a7b77b91f8c00b8d1444041a
fahdfady <fahd.fady212@gmail.com>
2025-02-21 14:04:33 +0200

    server instance

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,14 @@
 use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
-use crate::config::database_url;
 
 pub async fn connect() -> SqlitePool {
+    // Create the data directory if it doesn't exist
+    std::fs::create_dir_all("data").expect("Failed to create data directory");
+
+    // Construct the database URL using the data directory
+    let database_url = "sqlite:data/blog.db";
+    
     SqlitePoolOptions::new()
-        .connect(&database_url())
+        .connect(&database_url)
         .await
         .expect("Failed to connect to database")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+use blog_api::{
+    config::{SERVER_HOST, SERVER_PORT},
+    db, routes,
+};
+use sqlx::{migrate, Sqlite};
+use std::net::SocketAddr;
+
 #[tokio::main]
-async fn main(){
-    println!("hello world");
+async fn main() {
+    let pool: sqlx::Pool<Sqlite> = db::connect().await;
+
+    migrate!()
+        .run(&pool)
+        .await
+        .expect("unable to run database migrations");
+
+    let app: axum::Router = routes::create_router(pool);
+
+    let addr: SocketAddr = SocketAddr::new(SERVER_HOST.into(), SERVER_PORT);
+
+    println!("Server running at http://{}", addr);
+
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("server failed to start :(");
 }
