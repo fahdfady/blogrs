57c23109fdebe71123c83eab06abf81a966b7030
fahdfady <fahd.fady212@gmail.com>
2025-02-21 14:08:28 +0200

    fix: axum routers

--- src/routes/blog.rs
+++ src/routes/blog.rs
@@ -9,7 +9,7 @@ use crate::handlers::blog;
 pub fn router(pool: SqlitePool) -> Router {
     Router::new()
         .route("/blogs", get(blog::get_blogs).post(blog::create_blog))
-        .route("/blogs/:id", put(blog::update_blog))
-        .route("/blogs/:id/author", get(blog::get_blog_author))
+        .route("/blogs/{id}", put(blog::update_blog))
+        .route("/blogs/{id}/author", get(blog::get_blog_author))
         .with_state(pool)
 }
